a6243289b0f1a4a00b444ad02c2835468346ec60
phibkro <71797726+phibkro@users.noreply.github.com>
2025-02-11 19:00:33 +0100

    feat(ui): :poop: export button locally without styling

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -6,6 +6,23 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    lib: {
+      entry: {
+        button: path.resolve(__dirname, 'src/components/ui/button.tsx'),
+      },
+      name: 'ui'
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, 'src'),
